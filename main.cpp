a483024823acca9989a02844d8a1146702d0c5c2
Black Beard <67589385+blackbeard173@users.noreply.github.com>
2020-09-19 20:01:15 +0530

    initial commit

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h> 
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+/*
+*************************************
+FEATURES~
+
+    ->Master Entry
+        -> Member Entry
+        -> Computer Entry
+    ->Cafe Management
+        -> Booking
+        -> Charges 
+        -> Renewal
+        -> Return
+    ->Exit
+*************************************
+*/
+
+/*
+*************************************
+MODULES~
+    
+    -> Login process module
+    -> Master Entry for member
+    -> Master Entry For computers
+    -> Allotment of computers (Log In)
+    -> Log out Module
+    -> Charges Module
+    -> Renewal of Member Module
+*************************************
+*/
+
+int main()
+{
+    // cout << "Hello World";
+   return 0;
+}
